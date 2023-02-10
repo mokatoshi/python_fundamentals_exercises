a430281800ee9cb4083330aedd2be2bcb231bb46
mokatoshi <122816499+mokatoshi@users.noreply.github.com>
2023-02-10 15:13:13 +0200

    Delete fourth_exercise_3.02.2023 directory

--- fourth_exercise_3.02.2023/min_max_sum.py
+++
@@ -1,6 +0,0 @@
-digits = [int(x) for x in input().split()]
-
-print(f"The minimum number is {min(digits)}")
-print(f"The maximum number is {max(digits)}")
-print(f"The sum number is: {sum(digits)}")
-
